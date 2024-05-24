495e9a4e50b746bb6513ae971b2465bcf5940bd1
anni1236012 <anni1236012@gmail.com>
2024-05-24 18:44:48 -0500

    small update

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Get Credentials!!!!
+            Get Credentials!
           </h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
